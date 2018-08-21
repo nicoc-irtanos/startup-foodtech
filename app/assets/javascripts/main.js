e7c1c93ede71da802546939d8495e4b3387d8ee1
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-21 14:39:32 +0200

    tabs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,26 @@
-function next{
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+function next() {
 
 }
 
-function previous{
+function previous() {
   
 }
+$(document).ready(function() {
+  $('.tablinks:not(:first)').addClass('inactive');
+  $('.tabcontent').hide();
+  $('.tabcontent:first').show();
+  $('.tablinks').click(function(){
+    var t = $(this).attr('id');
+  
+    if($(this).hasClass('inactive')){ //this is the start of our condition 
+      $('.tablinks').addClass('inactive');           
+      $(this).removeClass('inactive');
+  
+      $('.tabcontent').hide();
+      $('#'+ t + 'c').show();
+   }
+  });
+});
